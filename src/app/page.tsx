03605ee2c7bd37fb638088aae219ff027c7b09eb
Daria Kuliashova <daria.kuliashova@scootapi.com>
2024-10-15 10:38:38 +0200

    D-1: add menu and homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
-export default function Home() {
-  return <main>Homepage</main>;
-}
+import { Button } from '@/components/ui/button';
+import { FiDownload } from 'react-icons/fi';
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span>Front End Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I&apos;m <br />
+              <span className="text-accent">Darya Kuliashova</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I am a front-end developer with three years of experience building
+              websites and web applications and proficient in various
+              programming languages and technologies.
+            </p>
+          </div>
+          <div className="flex flex-col xl:flex-row items-center gap-8">
+            <Button
+              variant="outline"
+              size="lg"
+              className="uppercase flex items-center gap-2"
+            >
+              <span>Download CV</span>
+              <FiDownload className="" />
+            </Button>
+            <div className="mb-8 xl:mb-0">social</div>
+          </div>
+
+          <div className="flex">photo</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { CiMenuFries } from 'react-icons/ci';
+
+const links = [
+  { name: 'Home', path: '/' },
+  { name: 'Services', path: '/services' },
+  { name: 'Resume', path: '/resume' },
+  { name: 'Work', path: '/work' },
+  { name: 'Contact', path: '/contact' },
+];
+
+const MobileNav = () => {
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h2 className="text-4xl font-semibold">
+              Darya <span className="text-accent">.</span>
+            </h2>
+          </Link>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${link.path === pathname && 'text-accent border-b-2 border-accent'} text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
