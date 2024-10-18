ea62724edc0426e2478af7e02c8defc6cbef44a1
Daria Kuliashova <daria.kuliashova@scootapi.com>
2024-10-18 15:11:14 +0200

    Fix component TS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
@@ -45,6 +46,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
 };
 export default config;
