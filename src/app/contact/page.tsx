142d6df74d8a7db8ba803f019c043b4d144f37df
Daria Kuliashova <daria.kuliashova@scootapi.com>
2024-10-18 10:37:25 +0200

    D-4: add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,84 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: 'Phone',
+    description: '+48698149204',
+  },
+  {
+    icon: <FaEnvelope />,
+    title: 'Email',
+    description: 'daria.kuliashova@gmail.com',
+  },
+  {
+    icon: <FaMapMarkedAlt />,
+    title: 'Address',
+    description: 'Białystok, Poland',
+  },
+];
+
 const Contact = () => {
-  return <div>Contact page</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:w-[54%] order-2 xl:order-none">
+            <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent"> Let&apos;s work together</h3>
+              <p className="text-white/60">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, totam excepturi? Eos minus molestiae
+                et?
+              </p>
+              {/* Input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="First name" />
+                <Input type="lastname" placeholder="Last name" />
+                <Input type="email" placeholder="Email" />
+                <Input type="phone" placeholder="Phone number" />
+              </div>
+              {/* Textarea */}
+              <Textarea className="h-[200px]" placeholder="Type your message here" />
+              {/* Button */}
+              <Button size="md" className="max-w-40">
+                Send message
+              </Button>
+            </form>
+          </div>
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => (
+                <li key={index} className="flex items-center gap-6">
+                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-justify-center">
+                    <div className="text-[28px]">{item.icon}</div>
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-white/60">{item.title}</p>
+                    <h3 className="text-xl">{item.description}</h3>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contact;
