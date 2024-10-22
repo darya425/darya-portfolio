1359d50c0401865139645fe9866d9f37a138e444
Daria Kuliashova <daria.kuliashova@scootapi.com>
2024-10-22 09:27:43 +0200

    add google txt

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -56,7 +56,6 @@ const Contact = () => {
           Accept: 'application/json',
         },
         body: JSON.stringify(formData),
-        credentials: 'include',
       });
 
       if (response.ok) {
