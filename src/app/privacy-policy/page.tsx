f9bbea49c01f0dad5133e20f5043f925e875caf3
Daria Kuliashova <daria.kuliashova@scootapi.com>
2024-10-22 10:11:39 +0200

    Add privacy police

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,45 @@
+const PrivacyPolicy = () => {
+  return (
+    <p>
+      Privacy Policy Effective Date: [22/10/2024] 1. Introduction Welcome to ["Darya Kuliashova Portfolio"]. This
+      Privacy Policy explains how we collect, use, and protect your personal data when you visit our website
+      [https://darya-kuliashova-portfolio.netlify.app] (hereinafter referred to as "the Site"). By using the Site, you
+      agree to the collection and use of information in accordance with this policy. 2. Information We Collect We may
+      collect and process the following types of information: Personal Data: Information that can identify you as an
+      individual, such as your name, email address, and other contact information when you voluntarily provide it via
+      forms on the Site (e.g., contact forms). Usage Data: Information about how you access and interact with the Site,
+      including your IP address, browser type, pages viewed, and the time you spend on the Site. This data is collected
+      through cookies and other tracking technologies. 3. How We Use Your Information We use the data collected for the
+      following purposes: To provide, maintain, and improve our services and content on the Site. To communicate with
+      you and respond to your inquiries. To monitor and analyze usage patterns and trends to improve the Site's
+      functionality and user experience. 4. Sharing Your Information We do not sell, trade, or otherwise transfer your
+      personally identifiable information to third parties, except in the following cases: Service Providers: We may
+      share your information with trusted third-party service providers who assist us in operating the Site, conducting
+      our business, or servicing you, provided that those parties agree to keep this information confidential. Legal
+      Obligations: We may disclose your information if required by law or in response to valid requests by public
+      authorities (e.g., a court or government agency). 5. Cookies and Tracking Technologies We use cookies and similar
+      tracking technologies to enhance your experience on the Site. Cookies are small files stored on your device that
+      allow us to recognize your browser and capture certain information. You can choose to disable cookies in your
+      browser settings, but some features of the Site may not function properly without them. 6. Security of Your
+      Information We take the security of your personal information seriously and implement appropriate technical and
+      organizational measures to protect it from unauthorized access, disclosure, alteration, or destruction. 7. Links
+      to Other Websites Our Site may contain links to other websites that are not operated by us. If you click on a
+      third-party link, you will be directed to that third party's website. We strongly advise you to review the Privacy
+      Policy of every site you visit, as we have no control over and assume no responsibility for the content or privacy
+      practices of any third-party sites or services. 8. Your Data Protection Rights Depending on your location, you may
+      have the following rights regarding your personal data: The right to access: You have the right to request copies
+      of your personal data. The right to rectification: You have the right to request correction of inaccurate or
+      incomplete data. The right to erasure: You have the right to request that we delete your personal data, under
+      certain conditions. The right to restrict processing: You have the right to request that we restrict the
+      processing of your personal data, under certain conditions. The right to object to processing: You have the right
+      to object to our processing of your personal data, under certain conditions. To exercise any of these rights,
+      please contact us at [your contact email]. 9. Changes to This Privacy Policy We may update this Privacy Policy
+      from time to time. Any changes will be posted on this page with an updated "Effective Date." We encourage you to
+      review this Privacy Policy periodically for any updates. 10. Contact Us If you have any questions or concerns
+      about this Privacy Policy or how we handle your personal data, please contact us at: Email:
+      [daria.kuliashova@gmail.com] Website: [https://darya-kuliashova-portfolio.netlify.app]
+    </p>
+  );
+};
+
+export default PrivacyPolicy;
